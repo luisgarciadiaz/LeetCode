c0eb41ab5d7682fed7f4b62e8c047bb347651d40
Luis García Díaz <zethes@gmail.com>
2025-09-05 07:50:50 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(string.Join(", ", arrays.TwoSum(new int[] { 2, 7, 11, 15 }, 9)));
+Console.WriteLine(string.Join(", ", arrays.TwoSum(new int[] { 3, 2, 4 }, 6)));
+Console.WriteLine(string.Join(", ", arrays.TwoSum(new int[] { 3, 3 }, 6)));
+Console.WriteLine("Hello, World!");
---
+++ arrays.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class arrays
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> seen = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (seen.TryGetValue(complement, out int index))
+            {
+                return new int[] { index, i };
+            }
+            seen[nums[i]] = i;
+        }
+        throw new InvalidOperationException("No valid two sum found.");
+    }
+}
