a69dfe3c4453806db1bddca645a2ab44b0da7c20
Luis García Díaz <zethes@gmail.com>
2025-09-06 15:13:32 -0600

    2

--- Program.cs
+++ Program.cs
@@ -2,7 +2,11 @@
 {
     private static void Main(string[] args)
     {
-       
+        ListNode l1= new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2= new ListNode(5, new ListNode(6, new ListNode(4,new ListNode(9))));
+        ListNode l3= new ListNode(0);
+
+        ListNode result=ListNode.AddTwoNumbers(l1,l2);
 
     }
 }
