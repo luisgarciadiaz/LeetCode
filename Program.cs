18eb15fb21cf8eb9ca040b858893a5fc9f12d43d
Luis García Díaz <zethes@gmail.com>
2025-09-06 15:22:10 -0600

    3

--- Program.cs
+++ Program.cs
@@ -2,11 +2,9 @@
 {
     private static void Main(string[] args)
     {
-        ListNode l1= new ListNode(2, new ListNode(4, new ListNode(3)));
-        ListNode l2= new ListNode(5, new ListNode(6, new ListNode(4,new ListNode(9))));
-        ListNode l3= new ListNode(0);
-
-        ListNode result=ListNode.AddTwoNumbers(l1,l2);
+        Console.WriteLine( LeetCode.strings.lengthofLongestSubstring("abcabcbb"));
+        Console.WriteLine(LeetCode.strings.lengthofLongestSubstring("bbbbb"));
+        Console.WriteLine(LeetCode.strings.lengthofLongestSubstring("pwwkew"));
 
     }
 }
---
+++ strings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class strings
+    {
+        public static int lengthofLongestSubstring(string s)
+        {
+            int n = s.Length;
+            int ans = 0;
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            for (int j = 0, i = 0; j < n; j++)
+            {
+                if (map.ContainsKey(s[j]))
+                {
+                    i = Math.Max(map[s[j]], i);
+                }
+                ans = Math.Max(ans, j - i + 1);
+                map[s[j]] = j + 1;
+            }
+            return ans;
+
+        }
+    }
+}
